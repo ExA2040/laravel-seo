59da35670e4930bd8912e380171908a0d4265c59
Ehsan <akaseiryu@gmail.com>
2016-12-20 16:22:42 +0330

    first

--- src/migrations/2016_06_06_060606_create_seo_table.php
+++ src/migrations/2016_06_06_060606_create_seo_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCounterTable extends Migration {
+class CreateSEOTable extends Migration {
 
   /**
    * Run the migrations.
